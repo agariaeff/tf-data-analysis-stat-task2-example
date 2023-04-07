62daef8eb95e67aa2771a151e8e3e53614b12635
agariaeff <96379727+agariaeff@users.noreply.github.com>
2023-04-07 23:28:27 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,17 +1,23 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+from scipy.stats import expon
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 540157708 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
+    n = len(x)
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    t_alpha_2 = expon.ppf(1 - alpha / 2)
+    loc = np.mean(x)
+    s2 = np.var(x, ddof=1)
+    se = np.sqrt(s2 / n)
+
+    lower = loc - se * t_alpha_2 / (2 * loc)
+    upper = loc + se * t_alpha_2 / (2 * loc)
+
+    return (lower, upper)
