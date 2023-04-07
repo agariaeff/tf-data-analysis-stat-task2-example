6ab7bb11f0eba08f520c0af42d5d629ee219c34a
agariaeff <96379727+agariaeff@users.noreply.github.com>
2023-04-08 00:47:49 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import expon
+import scipy.stats
 
 
 chat_id = 540157708 # Ваш chat ID, не меняйте название переменной
@@ -10,14 +10,20 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    n = len(x)
+    time = 38 
     alpha = 1 - p
-    t_alpha_2 = expon.ppf(1 - alpha / 2)
-    loc = np.mean(x)
-    s2 = np.var(x, ddof=1)
-    se = np.sqrt(s2 / n)
+    semians = 0
+    for i in range(len(x)):
+      semians += x[i] 
+    semians /= len(x) 
+    left = semians + scipy.stats.gamma.ppf((alpha)/2, len(x))/len(x) 
+    right = semians + scipy.stats.gamma.ppf(1-alpha/2, len(x))/len(x)
+    left -= 1/2
+    right -= 1/2
+    left /= (time**2)
+    right /= (time**2)
+    left *= 2
+    right *= 2
 
-    lower = loc - se * t_alpha_2 / (2 * loc)
-    upper = loc + se * t_alpha_2 / (2 * loc)
-
-    return (lower, upper)
+    return left, \
+           right
